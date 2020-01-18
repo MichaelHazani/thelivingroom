482fd0ff732f26cae2b3a3070f2e7fbe863e35a9
michaelhazani <123mickey@gmail.com>
2020-01-18 22:11:33 +0200

    timer, video switcher, debug GUI

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,116 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-cout << "hello raspi!" << endl;
+void ofApp::setup() {
+
+	scrWidth = ofGetScreenWidth();
+	scrHeight = ofGetScreenHeight();
+
+	startTime = ofGetElapsedTimeMillis();
+
+	DebugView = false;
+
+	refreshCurrentHour();
+	playVideo();
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+	clipPlayer.update();
+	updateTimer();
+}
+
+//--------------------------------------------------------------
+void ofApp::updateTimer()
+{
+	timer = ofGetElapsedTimeMillis() - startTime;
+	//every two seconds
+	if (timer >= 2000) {
+		startTime = ofGetElapsedTimeMillis();
+		refreshCurrentHour();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	clipPlayer.draw(0, 0, scrWidth, scrHeight);
+
+	//GUI
+	if (DebugView) {
+		ofDrawBitmapString("Current Hour: " + ofToString(currHour), 10, 10);
+		ofDrawBitmapString("Current Timer: " + ofToString(timer), 10, 30);
+		//ofDrawBitmapString("Counter: " + ofToString(sch.counter), 10, 20);
+	}
+}
+
+void ofApp::refreshCurrentHour() {
+	if (currHour != ofGetHours())
+	{
+		currHour = ofGetHours();
+		playVideo();
+	}
+}
+
+void ofApp::playVideo() {
+	clipPlayer.load("loops/" + ofToString(ofGetHours()) + ".mp4");
+	clipPlayer.play();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	if (key == ' ')
+	{
+		DebugView = !DebugView;
+	}
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,21 +2,55 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
-	public:
-		void setup();
-		void update();
-		void draw();
-		
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+//class Scheduler : public ofThread {
+//public:
+//	unsigned long counter = 0;
+//
+//	Scheduler() {
+//		timer.setPeriodicEvent(1000000000); // 1 second
+//		startThread();
+//	}
+//private:
+//	ofTimer timer;
+//	void threadedFunction() {
+//		while (isThreadRunning()) {
+//			timer.waitNext();
+//			counter++;
+//		}
+//	}
+//};
+
+class ofApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	// custom methods and variables
+	bool DebugView;
+	float scrWidth;
+	float scrHeight;
+	int currHour;
+	float startTime;
+	float timer;
+
+	ofVideoPlayer clipPlayer;
+
+	void refreshCurrentHour();
+	void playVideo();
+	void updateTimer();
+
+	//Scheduler sch;
 };
